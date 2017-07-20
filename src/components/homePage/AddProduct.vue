4d64d8f298eb0f652d2da53d64fe060c2c9d1537
6337215 <1130289946@qq.com>
2017-07-20 14:53:00 +0800

    wushihao
    新增了首页的页面

---
+++ src/components/homePage/AddProduct.vue
@@ -0,0 +1,81 @@
+<template>
+	<div class="add_product" >
+		<div class="pro_pic"><router-link to="/"><img :src="allmes.img"/></router-link></div>
+		<div><router-link to="/">{{allmes.title}}</router-link></div>
+		<div>
+				<div><span>{{allmes.cost}}</span><span>{{allmes.cost2}}</span></div>
+				<router-link to="/"><img src="../../../static/homePage/index_flow.png"/></router-link>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+//		props:['img'],
+		props:['allmes'],
+//		props:['cost'],
+//		props:['costOld'],
+		data() {
+	      return {
+//	        img:'../../../static/homePage/product.jpg',
+//	        title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',
+//	        cost:'1268.00',
+//	        costOld:'321.60',
+//	        allmes:{img:'../../../static/homePage/product.jpg',cost:'1268.00', costOld:'321.60',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装'}
+	      }
+	  },		
+		components: {
+		    
+		  }
+	}	
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+.add_product
+	width 100%
+	div:nth-child(1)
+		a
+			img
+				width 100%
+	div:nth-child(2)
+		width 93%
+		margin auto
+		border-bottom 0.0133rem solid #eeeeee
+		a
+			
+			color #686868
+			font-size 0.32rem
+			display block
+			width 100%
+			padding 0.1866rem 0 0.266rem
+			/*text-align center*/
+	div:last-child		
+		position relative
+		height 1.2rem
+		div:nth-child(1)
+			position absolute
+			width 1.5333rem
+			left 0.1733rem
+			top 0.2666rem
+			span:nth-child(1)
+				display block
+				font-size 0.3733rem
+				font-weight bold
+				color #E71F19
+			span:last-child
+				color #999999
+				font-size 0.24rem
+				text-decoration line-through
+		a:nth-child(2)
+			position absolute 
+			width 0.7066rem
+			height 0.7066rem
+			right 0.1466rem
+			top 0.2666rem
+			background-color #e71f19
+			border-radius 50%
+			img
+				width 100%
+				
+			
+</style>
--- src/components/homePage/HomePage.vue
+++ src/components/homePage/HomePage.vue
@@ -1,15 +1,323 @@
 <template>
-    <div>
-        {{i}}
+    <div class="homepage">
+     	<div class="header">
+     		<router-link to="/classify"><img src="../../../static/homePage/classify_icon.png"/></router-link>
+     		<span>茶叶电子专柜</span>
+     		<router-link to="/classify"><img src="../../../static/homePage/cart_icon.png"/></router-link>
+     	</div>
+     	<home-swiper v-bind:banner="swiperImg"></home-swiper>
+     	<div class="nav_bar">
+     		<!--输入框-->
+     		<div class="search">
+     			<input type="text" placeholder="请输入你所搜索的商品"/>
+     			<img src="../../../static/homePage/icosousuo.png"/>
+     		</div>
+     		<!--导航栏列表图标-->
+     		<div class="nav_logo">
+     			<ul>
+     				<li v-for="(item,index) in logoImg" @click="navFn(index)">
+     					<img :src="item.img"/>
+     					<p>{{item.title}}</p>
+     				</li>
+     			</ul>
+     		</div>
+     	</div>
+     	<div class="nav_pic">
+     		<img src="../../../static/homePage/nav_pic.png" />
+     	</div>
+     	<!--中间方格布局，品牌展示-->
+     	<div class="brand">
+     		<ul>
+				<li v-for="val in brandImg"><img :src="i.img" v-for="i in val" /></li>
+     		</ul>
+     	</div>
+     	<div class="product">
+     		<!--新品上市-->
+	     	<div class="new_product">
+	     		<div class="nav_top">
+	     			<span>新品上市</span>
+	     			<router-link to="/">更多></router-link>
+	     		</div>
+	     		<new-swiper v-bind:banners="newProd"></new-swiper>
+	     	</div>
+	     	<!--热销商品-->
+	     	<div class="new_product">
+	     		<div class="nav_top">
+	     			<span>热销商品</span>
+	     			<router-link to="/">更多></router-link>
+	     		</div>
+	     		<new-swiper v-bind:banners="hotProd"></new-swiper>
+	     	</div>
+	     	<!--乌龙茶-->
+	     	<div class="new_product">
+	     		<div class="nav_top">
+	     			<span>乌龙茶</span>
+	     			<router-link to="/">更多></router-link>
+	     		</div>
+	     		<div class="oolong">
+	     			<ul v-for="m in oolongProd">
+	     				<li><add-product v-bind:allmes="m" ></add-product> </li>
+	     			</ul>	     			
+	     		</div>
+	     	</div>
+	     	<!--精品推荐-->
+	     	<h2>———— 精品推荐 ————</h2>
+	     	<div class="recommend">
+	     		<ul v-for="n in recomProd">
+	     			<li><add-product v-bind:allmes="n" ></add-product></li>
+	     		</ul>
+	     	</div>
+     	</div>
 
-    </div>
 
+    </div>	
 </template>
 
 <script>
+import HomeSwiper from "./HomeSwiper"
+import AddProduct from "./AddProduct"
+import NewSwiper from "./NewSwiper"
+//import AddProduct from "./AddProduct"
+
+export default {
+		//渲染数据
+		data: function() {
+			return {
+				swiperImg:["../../../static/homePage/swiper1.jpg","../../../static/homePage/swiper2.jpg","../../../static/homePage/swiper3.jpg"],
+				//logo图标
+				logoImg:[
+					{img:'../../../static/homePage/nav_logo1.png',title:'商品分类'},
+					{img:'../../../static/homePage/nav_logo2.png',title:'促销活动'},
+					{img:'../../../static/homePage/nav_logo3.png',title:'优惠活动'},
+					{img:'../../../static/homePage/nav_logo4.png',title:'拼团'},
+					{img:'../../../static/homePage/nav_logo5.png',title:'附近店铺'},
+					{img:'../../../static/homePage/nav_logo6.png',title:'砍价'},
+					{img:'../../../static/homePage/nav_logo7.png',title:'预售'},
+					{img:'../../../static/homePage/nav_logo8.png',title:'一元云购'},
+					{img:'../../../static/homePage/nav_logo9.png',title:'积分商城'},
+					{img:'../../../static/homePage/nav_logo10.png',title:'商家'}
+				],
+				//中间方格的图标
+				brandImg:[
+					[{img:'../../../static/homePage/brand1.jpg'}],
+					[{img:'../../../static/homePage/brand2.png'},{img:'../../../static/homePage/brand3.png'}],
+					[{img:'../../../static/homePage/brand4.png'},{img:'../../../static/homePage/brand6.png'}],
+					[{img:'../../../static/homePage/brand5.png'},{img:'../../../static/homePage/brand7.png'}]
+				],
+				//新品上市
+				newProd:	[
+		      		[{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'}],
+		      		[{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'}]
+		      ],
+		      //商品热销
+				hotProd:	[
+		      		[{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'}],
+		      		[{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'}]
+		      ],
+		      //乌龙茶
+		      	oolongProd:[
+		      		{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},
+		      		{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},
+		      		{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},
+		      		{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},
+		      		{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},
+		      		{img:'../../../static/homePage/product.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'}
+		      	],
+		      	recomProd:[
+		      		{img:'../../../static/homePage/product1.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},
+		      		{img:'../../../static/homePage/product1.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},
+		      		{img:'../../../static/homePage/product1.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},
+		      		{img:'../../../static/homePage/product1.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},
+		      		{img:'../../../static/homePage/product1.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'},
+		      		{img:'../../../static/homePage/product1.jpg',title:'传承老树白茶9018 福鼎白牡丹2016春节送礼佳品白茶套装',cost:'1268.00',cost2:'321.60'}
+		      	],
+		      	
+		      	
+				
+			}
+		},
+		//注册组件
+		components:{
+			HomeSwiper,
+			AddProduct,
+			NewSwiper,
+			AddProduct
+		},
+		methods:{
+			navFn(index){
+				switch (index){
+					case 0:
+//						console.log(this.$router);
+						this.$router.push({name:'ClassifyPage'});
+						break;
+					case 1:
+						alert(2);
+						break;
+				}
+			}
+		}
+	}
 
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+.homepage
+	/*导航栏*/
+	.header
+		background-color #e31939
+		height 1.2133rem
+		line-height 1.2133rem
+		text-align center
+		a
+			float left		
+			img
+				width 0.7066rem 
+				height 0.7066rem	
+				vertical-align middle		
+		span
+			font-size 0.48rem
+			color white
+			text-align center
+		a:nth-child(1)
+			float left
+			padding-left 0.4rem	
+		a:last-child
+			float right
+			padding-right 0.4rem
+	.nav_bar
+		width 9.2rem
+		margin 0.5066rem auto 0
+		/*搜索框*/
+		.search
+			width 100%
+			border 1px solid #999999
+			border-radius 0.1333rem
+			input
+				width 80%
+				font-size 0.3733rem
+				color #999999
+				padding 0.2833rem 
+				border 0
+				border-right 0.0266rem solid #999  
+			img
+				width 0.76rem
+				height 0.72rem
+				padding 0.12rem 0
+				vertical-align middle 
+		/*十个小图标*/
+		.nav_logo
+			margin-top 0.44rem
+			ul
+				list-style none 
+				margin 0
+				padding 0
+				li
+					display inline-block
+					width 1.8133rem
+					margin-bottom 0.3733rem	
+					height 1.8666rem
+					text-align center
+					img
+						width 1.2533rem
+						height 1.2533rem
+						margin 0 auto
+					p
+						font-size 0.3066rem
+						margin-top 0.2533rem
+	.nav_pic
+		width 100%
+		img
+			width 100%
+			height 2.88rem
+	/*中间方格布局，品牌展示*/
+	.brand
+		width 100%
+		ul
+			list-style none
+			margin 0
+			padding 0
+			
+			li
+				box-sizing border-box
+				border 1px solid white
+				display inline-block
+				width 50%
+				height 5.52rem
+				vertical-align top
+				img
+					width 100%
+					height 2.76rem
+					/*vertical-align top*/	
+			li:nth-child(1)
+				img
+					height 5.52rem
+	/*商品列表*/
+	.product
+		background-color #eee
+		.new_product
+			width 100%
+			margin-bottom 0.28rem
+			background-color #fff
+			.nav_top
+				width 100%
+				height 1.1466rem
+				line-height 1.1466rem
+				border 0.0133rem solid #eee
+				margin-bottom 0.3466rem
+				span:nth-child(1)
+					display inline-block
+					width 46%
+					font-size 0.3866rem
+					padding-left 2%
+					/*font-weight bold*/
+					border-left 0.0533rem solid #e71f19
+				a:nth-child(2)
+					display inline-block
+					padding-right 2%
+					width 46%
+					text-align right	
+					font-size 0.3866rem			
+					color #999999
+					text-decoration none
+			.oolong
+				ul
+					width 33%
+					display inline-block
+					overflow hidden
+					margin-bottom 0.1333rem
+					li						
+						width 92%
+						margin 0 4%
+						height 6.1333rem
+		h2
+			height 1.3733rem
+			line-height 1.3733rem
+			text-align center					
+			font-size 0.44rem
+			color #999	
+		.recommend
+			background-color #fff
+			width 96%
+			margin 0 2%
+			ul
+				display inline-block
+				width 49%
+				/*margin 0*/
+				padding 0
+				list-style none
+			ul:nth-child(2n-1)
+				margin 0 1%
+				
+											
+.show{
+	width:0.0266rem;
+}		
 
+/*.clearfix:after
+	content " "
+	clear both
+	width 0
+	height 0
+	visibility hidden
+	display block		*/
 </style>
